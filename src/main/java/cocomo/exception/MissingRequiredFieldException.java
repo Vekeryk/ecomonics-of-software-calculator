f22cc169192d024144fbcb7d3eeea940a61f2209
Denys Vekeryk <den.vekeryk@gmail.com>
2022-11-07 17:37:36 +0200

    Customised MissingRequiredFieldException

--- src/main/java/cocomo/exception/MissingRequiredFieldException.java
+++ src/main/java/cocomo/exception/MissingRequiredFieldException.java
@@ -1,4 +1,8 @@
 package cocomo.exception;
 
 public class MissingRequiredFieldException extends RuntimeException {
+    
+    public MissingRequiredFieldException(String message) {
+        super(message);
+    }
 }
